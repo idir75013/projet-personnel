152bed4f8f4aee1185dc043503459299624f9acd
Idir Temine <idir.temine@gmail.com>
2018-03-20 19:18:57 +0100

    création d'un formulaire

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+router.get('/create', function(req, res, next) {
+  res.render('user/create', {title: 'créer un nouvel utilisateur', errors:[]});
 });
+
 
 module.exports = router;
